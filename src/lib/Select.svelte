7c92ead05c1ee3c476128c67263f51507edba165
Josh Hardy <jks.hardy@gmail.com>
2022-10-25 11:41:17 +0100

    select component updates

--- src/lib/Select.svelte
+++ src/lib/Select.svelte
@@ -1,46 +1,37 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+	type Item = {
+		label: string;
+		value?: any;
+	};
 
-  type Item = {
-    label: string,
-    value?: any
-  }
+	export let items: Item[] = [];
+	export let value: any = -1;
 
-  export let items:Item[] = [];
-  export let value:Item= items[0];
+	export let disabled = false;
 
-  export let disabled = false;
-
-  const dispatch = createEventDispatcher();
-
-	$: disabledClass = disabled ? 'disabled' : "";
-
-  export const validate = () => {
-    return {
-      ok: true,
-      value,
-    };
-  };
-
+	export const validate = () => {
+		return {
+			ok: true,
+			value
+		};
+	};
 </script>
 
-
 <div class="flex w-full flex-col gap-y-2">
-  <div class="self-start rounded-md border border-gray-500 text-white ">
-    <select
-      {disabled}
-      bind:value
-      on:change={() => {
-        dispatch("change");
-      }}
-      class={`default text-light text-black dark:text-white ${disabledClass}`}
-    >
-        <option class="opacity-50" value="" hidden selected >Select</option>
-      {#each items as item}
-      <option class="text-black" value={item}>{item.label}</option>
-    {/each}
-    </select>
-  </div>
+	<div class="self-start rounded-md border border-gray-500 text-white ">
+		<select
+			{disabled}
+			bind:value
+			on:change
+			class="default text-light text-black dark:text-white"
+			class:disabled
+		>
+			<option class="opacity-50" value={-1} hidden selected>Select</option>
+			{#each items as item, i}
+				<option class="text-black" value={item?.value || i}>{item.label}</option>
+			{/each}
+		</select>
+	</div>
 </div>
 
 <style lang="postcss">
@@ -48,7 +39,7 @@
 		@apply outline-none mr-2 border-none bg-transparent px-4 py-2;
 	}
 
-  .disabled {
+	.disabled {
 		@apply opacity-50 cursor-not-allowed;
 	}
 </style>
--- src/routes/examples/select/+page.svelte
+++ src/routes/examples/select/+page.svelte
@@ -6,46 +6,98 @@
 	import PageHeading from '$lib/_docs/PageHeading.svelte';
 	import Select from '$lib/Select.svelte';
 
-	
 	const options = [
-		{label: 'Option 1'},
-		{label: 'Option 2'},
-		{label: 'Option 3'},
-		{label: 'Option 4'},
+		{ label: 'Option 1' },
+		{ label: 'Option 2' },
+		{ label: 'Option 3' },
+		{ label: 'Option 4' }
 	];
 
-	let select1 = {label: "No option selected"};
-	let select2 = {label: "No option selected"};
+	const options2 = [
+		{ label: 'Option 1', value: 'opt1' },
+		{ label: 'Option 2', value: 'opt2' },
+		{ label: 'Option 3', value: 'opt3' },
+		{ label: 'Option 4', value: 'opt4' }
+	];
 
+	let select1 = -1,
+		select2: any = -1,
+		select3;
 </script>
 
 <div class="flex flex-col gap-y-4">
 	<PageHeading>Select</PageHeading>
 
-	<ExampleHeading>Regular</ExampleHeading>
+	<ExampleHeading>Labels only</ExampleHeading>
+	<div>
+		If no value is provided in the options array, the index will be used. Value is -1 before any
+		selection is made.
+	</div>
 	<Example>
 		<ExampleComponent>
-			<Select bind:value={select1} items={options} />
-			<div class="pt-4"><strong>Option selected:</strong> {select1.label}</div>
+			<Select bind:value={select2} items={options2} />
+			<div class="pt-4">
+				<strong>Option selected:</strong>
+				{options[select1]?.label || 'none selected'}
+			</div>
+			<div><strong>Option value:</strong> {select2}</div>
 		</ExampleComponent>
 		<ExampleUsage>
-{`<script>
-	const options = [...];
+			{`<script>
+	const options = [
+		{ label: 'Option 1' },
+		{ label: 'Option 2' },
+		{ label: 'Option 3' },
+		{ label: 'Option 4' }
+	];
 	let select;
 </script>
 
 <div>
 	<Select bind:value={select} items={options} />
+	<strong>Option label:</strong>{options[select]?.label || 'none selected'}
+	<strong>Option value:</strong> {select}
 </div>`}
 		</ExampleUsage>
 	</Example>
+
+	<ExampleHeading>Labels and values</ExampleHeading>
+	<Example>
+		<ExampleComponent>
+			<Select bind:value={select2} items={options2} />
+			<div class="pt-4">
+				<strong>Option selected:</strong>
+				{options2.find((opt) => opt?.value == select2)?.label || 'none selected'}
+			</div>
+			<div><strong>Option value:</strong> {select2}</div>
+		</ExampleComponent>
+		<ExampleUsage>
+			{`<script>
+	const options = [
+		{ label: 'Option 1', value: 'opt1' },
+		{ label: 'Option 2', value: 'opt2' },
+		{ label: 'Option 3', value: 'opt3' },
+		{ label: 'Option 4', value: 'opt4' }
+	];
+	let select;
+</script>
+
+<div>
+	<Select bind:value={select} items={options} />
+	<strong>Option selected:</strong>
+	{options.find((opt) => opt?.value == select)?.label || 'none selected'}
+	<strong>Option value:</strong> {select}
+</div>`}
+		</ExampleUsage>
+	</Example>
+
 	<ExampleHeading>Disabled</ExampleHeading>
 	<Example>
 		<ExampleComponent>
 			<Select bind:value={select2} disabled={true} items={options} />
 		</ExampleComponent>
 		<ExampleUsage>
-{`<script>
+			{`<script>
 	const options = [...];
 	let select;
 </script>
