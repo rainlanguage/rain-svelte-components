29c4e16ab36ec49e0b2c8d453ffd4a8d33d4772e
NanezX <victor.nanezj@gmail.com>
2022-10-27 22:22:24 -0400

    adding exports to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,11 @@ export { default as Switch } from './Switch.svelte';
 export { default as Button } from './Button.svelte';
 export { default as Input } from './Input.svelte';
 export { default as Ring } from './Ring.svelte';
+export { default as DisplayAddress } from './DisplayAddress.svelte';
+export { default as GenericSkeletonLoader } from './GenericSkeletonLoader.svelte';
+export { default as HoverTooltip } from './HoverTooltip.svelte';
+export { default as Jazzicon } from './Jazzicon.svelte';
+export { default as Select } from './Select.svelte';
 
 // parser stuff
 export { default as Parser } from './parser/Parser.svelte';
