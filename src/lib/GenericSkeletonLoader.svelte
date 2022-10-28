38a8625689c5f5db1bd867b4f51b058521c08f2d
NanezX <victor.nanezj@gmail.com>
2022-10-27 22:19:38 -0400

    display address component

---
+++ src/lib/GenericSkeletonLoader.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let show: any ="";
+  export let flexGrow: any = "";
+
+  $: wrapperClasses = flexGrow ? "relative flex-1" : "relative";
+
+  $: showContent = show
+    ? "transition-opacity duration-500 opacity-100"
+    : "transition-opacity duration-500 opacity-0";
+  $: showLoader = show
+    ? "absolute bg-gray-200 inset-0 rounded-2xl transition-opacity duration-1000 opacity-0"
+    : "absolute bg-gray-200 inset-0 rounded-2xl transition-opacity duration-300 animate-pulse";
+</script>
+
+<div class={wrapperClasses}>
+  <div class={showContent}>
+    <slot />
+  </div>
+  {#if !show}
+    <div class={showLoader} />
+  {/if}
+</div>
---
+++ src/routes/examples/display-address/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import ExampleComponent from '$lib/_docs/ExampleComponent.svelte';
+	import ExampleUsage from '$lib/_docs/ExampleUsage.svelte';
+	import Example from '$lib/_docs/Example.svelte';
+	import ExampleHeading from '$lib/_docs/ExampleHeading.svelte';
+	import PageHeading from '$lib/_docs/PageHeading.svelte';
+	import DisplayAddress from '$lib/DisplayAddress.svelte';
+	import Input from '$lib/Input.svelte';
+
+	let address: string = "0xabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";
+
+</script>
+
+<div class="flex flex-col gap-y-4">
+	<PageHeading>Display Address</PageHeading>
+	<div></div>
+	<ExampleHeading>Regular</ExampleHeading>
+	<Example>
+		<ExampleComponent>
+			<Input bind:value={address} debounce debounceTime={100}>
+				<span slot="label">Try to enter an address:</span>
+			</Input>
+			<div class="pt-4">
+				<strong>The address displayed: </strong>
+				<DisplayAddress {address}/>
+			</div>
+		</ExampleComponent>
+		<ExampleUsage>{`<DisplayAddress address={yourAddress}/>`}</ExampleUsage>
+	</Example>
+</div>
+
+<style>
+	.mb-5px {
+		margin-bottom: 5px;
+	}
+</style>
