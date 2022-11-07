2e1a1a5b0e7d573af018ac31edd6c1bc6d81a2c0
Josh Hardy <jks.hardy@gmail.com>
2022-11-07 09:53:49 +0000

    making href on tab a element optional

--- src/lib/tabs/Tab.svelte
+++ src/lib/tabs/Tab.svelte
@@ -1,35 +1,33 @@
-<script>
-  import { getContext } from "svelte";
-  import { TABS } from "./Tabs.svelte";
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
 
-  export let href = '';
+	export let href: string;
 
-  const tab = {};
-  const { registerTab, selectTab, selectedTab } = getContext(TABS);
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS) as any;
 
-  registerTab(tab);
-
-
+	registerTab(tab);
 </script>
 
-
-  <a
-    class={`tab ${$selectedTab === tab ? "selected dark:text-white dark:border-white dark:hover:border-white" : ""}`}
-    class:selected={$selectedTab === tab}
-    on:click={() => selectTab(tab)}
-    {href}
-  >
-    <slot />
-  </a>
-<div class="w-5 px-0"></div>
+<a
+	class={`tab ${
+		$selectedTab === tab ? 'selected dark:text-white dark:border-white dark:hover:border-white' : ''
+	}`}
+	class:selected={$selectedTab === tab}
+	on:click={() => selectTab(tab)}
+	href={href || null}
+>
+	<slot />
+</a>
+<div class="w-5 px-0" />
 
 <style lang="postcss">
-  .tab {
-    @apply cursor-pointer pb-2 px-3 font-sans tracking-tight text-gray-500 hover:border-gray-400 hover:border-b;
-  }
+	.tab {
+		@apply cursor-pointer pb-2 px-3 font-sans tracking-tight text-gray-500 hover:border-gray-400 hover:border-b;
+	}
 
-  .selected {
-    @apply border-b text-black border-black hover:border-black;
-  }
-
+	.selected {
+		@apply border-b text-black border-black hover:border-black;
+	}
 </style>
