9467c2476a0502a3811cba98a41f7b93fb82b557
NanezX <victor.nanezj@gmail.com>
2022-11-08 20:58:15 -0400

    Fix dark mode in tabs

--- src/lib/tabs/Tab.svelte
+++ src/lib/tabs/Tab.svelte
@@ -11,9 +11,7 @@
 </script>
 
 <a
-	class={`tab ${
-		$selectedTab === tab ? 'selected dark:text-white dark:border-white dark:hover:border-white' : ''
-	}`}
+	class={`tab`}
 	class:selected={$selectedTab === tab}
 	on:click={() => selectTab(tab)}
 	href={href || null}
@@ -22,12 +20,12 @@
 </a>
 <div class="w-5 px-0" />
 
-<style lang="postcss">
-	.tab {
+<style lang="postcss" global>
+	:local(.tab) {
 		@apply cursor-pointer pb-2 px-3 font-sans tracking-tight text-gray-500 hover:border-gray-400 hover:border-b;
 	}
 
-	.selected {
-		@apply border-b text-black border-black hover:border-black;
+	:local(.selected) {
+		@apply border-b text-black border-black hover:border-black dark:text-white dark:border-white dark:hover:border-white;
 	}
 </style>
