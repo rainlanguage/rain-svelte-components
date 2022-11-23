bbd1513e3fef22c4747aca423c2a0e6e4a9c75b7
Josh Hardy <jks.hardy@gmail.com>
2022-11-23 18:47:42 +0000

    removing log

--- src/lib/parser/parserHelpers.ts
+++ src/lib/parser/parserHelpers.ts
@@ -27,7 +27,6 @@ export const deserialize = (string: string) => {
 export const getFlatRanges = (value: Node[]) => {
     const text = serialize(value);
     const tree = Parser.getParseTree(text, rainterpreterOpMeta);
-    console.log(tree)
     const ranges: any[] = [];
     if (!Object.keys(tree).length) return ranges;
 
