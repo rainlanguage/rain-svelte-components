5fbe7c4e08f2b06f7813750c5e1a46331dcb4b57
NanezX <victor.nanezj@gmail.com>
2022-12-07 00:28:04 -0400

    roll back formatter

--- src/lib/formatter/Formatter.svelte
+++ src/lib/formatter/Formatter.svelte
@@ -6,10 +6,9 @@
 	export let readOnly: boolean = true;
 	export let raw: string | null = null;
 	export let stateConfig: StateConfig | null = null;
-	export let padding: string | null = null;
-  let padding_class:string
 
 	let formatter: ParserInput;
+
 	let showFormatter: boolean = true;
 
 	onMount(() => {
@@ -17,16 +16,10 @@
 		if (raw) formatter.loadRaw(raw.toLowerCase());
 		if (!raw && !stateConfig) showFormatter = false;
 	});
-
-	if (!padding) {
-		padding = 'p-4';
-	} else {
-		padding_class = `p-[${padding}]`;
-	}
 </script>
 
 {#if showFormatter}
-	<div class={`bg-gray-100 dark:bg-gray-800 rounded-lg ${padding_class}`}>
+	<div class="bg-gray-100 dark:bg-gray-800 rounded-lg p-4">
 		<ParserInput {readOnly} bind:this={formatter} />
 	</div>
 {/if}
