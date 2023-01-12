f998440b9f53cfaad1c678a18928cf2c371fbda6
Josh Hardy <jks.hardy@gmail.com>
2023-01-12 14:16:01 +0000

    implementing fork button and optional max height for formatter

--- src/lib/formatter/Formatter.svelte
+++ src/lib/formatter/Formatter.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
 	import ParserInput from '$lib/parser/ParserInput.svelte';
-	import { onMount } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 	import { Formatter, rainterpreterOpMeta, type StateConfig } from '@rainprotocol/rainlang';
+	import Button from '$lib/Button.svelte';
+	import { DocumentDuplicate } from '@steeze-ui/heroicons';
 
 	export let readOnly: boolean = true;
 	export let raw: string | null = null;
 	export let stateConfig: StateConfig | null = null;
+	export let showFork: boolean = true;
+	export let maxHeight: string | null = null;
 
 	let formatter: ParserInput;
 
 	let showFormatter: boolean = true;
+
+	const dispatch = createEventDispatcher();
 
 	onMount(() => {
 		if (stateConfig) raw = Formatter.get(stateConfig, { opmeta: rainterpreterOpMeta });
 		if (raw) formatter.loadRaw(raw.toLowerCase());
 		if (!raw && !stateConfig) showFormatter = false;
 	});
+
+	const handleFork = () => {
+		dispatch('fork');
+	};
 </script>
 
 {#if showFormatter}
-	<div class="bg-gray-100 dark:bg-gray-800 rounded-lg p-4">
-		<ParserInput {readOnly} bind:this={formatter} />
+	<div class="bg-gray-100 dark:bg-gray-800 rounded-lg relative">
+		<div class="overflow-y-scroll p-4" style={maxHeight ? `max-height: ${maxHeight}` : ''}>
+			<ParserInput {readOnly} bind:this={formatter} />
+		</div>
+		{#if showFork}
+			<div class="bottom-0 right-0 p-2 absolute bg-gray-100 dark:bg-gray-800">
+				<Button icon={DocumentDuplicate} on:click={handleFork} size="small">Fork</Button>
+			</div>
+		{/if}
 	</div>
 {/if}
--- src/routes/examples/formatter/+page.svelte
+++ src/routes/examples/formatter/+page.svelte
@@ -8,7 +8,78 @@
 	import { Parser } from '@rainprotocol/rainlang';
 	import { arrayify, hexlify } from 'ethers/lib/utils';
 
-	let raw = '_: add(2 3)';
+	let raw = `/**
+ ** Calculate the maximum number of NFTs to mint
+ */
+max-nfts: 100,
+
+/**
+ * Create a storage key.
+ */
+storage-key: hash(1234),
+
+/**
+ * Get the total number of NFTs minted
+ */
+total-minted: get(storage-key),
+
+/**
+ * Check if the maximum number of NFTs has been reached
+ */
+: ensure(less-than(total-minted max-nfts)),
+
+/**
+ * Increment the total number of NFTs minted
+ */
+: set(storage-key add(total-minted 1)),
+
+/**
+ * Create the ID for this NFT
+ */
+nft-id: add(total-minted 1),
+
+/**
+ * Get the sender of the transaction
+ */
+sender-address: context<0 0>(),
+
+/**
+ * Separator values 
+ */
+transferSeparator: 0xfea74d0c9bf4a3c28f0dd0674db22a3d7f8bf259c56af19f4ac1e735b156974f,
+mintBurnSeparator: 0xf339171dab445c29f9897dda2f42413426ee907dc7f8b52bd387bc7cf9384c6b,
+
+/**
+ * erc1155 transfers
+ */
+transferERC1155sList: transferSeparator,
+
+/**
+ * erc721 transfers
+ */
+transferERC721sList: transferSeparator,
+
+/**
+ * er20 transfers
+ */
+transferERC20sList: transferSeparator,
+
+/**
+ * native (gas) token transfers
+ */
+transferNativesList: transferSeparator,
+
+/**
+ * burns of this erc721 token
+ */
+burnsList: mintBurnSeparator,
+
+/**
+ * mints of this erc721 token
+ */
+mintsList: mintBurnSeparator,
+_ _: sender-address nft-id;
+`;
 
 	let stateConfig = {
 		constants: [2, 3],
@@ -44,7 +115,7 @@
 	<ExampleHeading>With raw text</ExampleHeading>
 	<Example>
 		<ExampleComponent>
-			<Formatter {raw} />
+			<Formatter {raw} maxHeight="400px" />
 		</ExampleComponent>
 		<ExampleUsage>
 			{`<script>
