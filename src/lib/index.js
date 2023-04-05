ffbce6fffa9bcfe6c74f925a54adbeedcfb08236
Josh Hardy <jks.hardy@gmail.com>
2023-04-05 13:59:47 +0100

    Revert "index update"
    This reverts commit 969e5b5d599e52944a8767c62b5caff774736ad2.

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,10 +2,6 @@ import { Tab, TabList, TabPanel, Tabs } from './tabs/tabs';
 export { Tab, TabList, TabPanel, Tabs };
 import { FilterGroup, FilterRow, FilterSet } from './filter/filter';
 export { FilterGroup, FilterRow, FilterSet };
-import { OverflowMenu, OverflowMenuItem } from './overflow-menu';
-export { OverflowMenu, OverflowMenuItem };
-import { Section, SectionBody, SectionHeading } from './section';
-export { Section, SectionBody, SectionHeading };
 
 export { default as Switch } from './Switch.svelte';
 export { default as Button } from './Button.svelte';
@@ -17,12 +13,8 @@ export { default as HoverTooltip } from './HoverTooltip.svelte';
 export { default as Jazzicon } from './Jazzicon.svelte';
 export { default as Select } from './Select.svelte';
 export { default as Modal } from './Modal.svelte';
-export { default as Pills } from './Pills.svelte';
-export { default as Formatter } from './formatter/Formatter.svelte';
-// abi form
-export { default as AutoAbiFormSeparated } from './auto-abi-form/AutoAbiFormSeparated.svelte';
+
 // parser stuff
 export { default as Parser } from './parser/Parser.svelte';
 export { default as OpDocs } from './parser/OpDocs.svelte';
 export { default as ParserInput } from './parser/ParserInput.svelte';
-export { default as SimulatedOutput } from './parser/SimulatedOutput.svelte';
