16b3bf521e2f7bfabe0e5072b12817cb54baef23
Josh Hardy <jks.hardy@gmail.com>
2023-04-05 10:49:04 +0100

    adding filter components to export

---
+++ src/lib/filter/filter.js
@@ -0,0 +1,3 @@
+export { default as FilterGroup } from './FilterGroup.svelte';
+export { default as FilterRow } from './FilterGroup.svelte';
+export { default as FilterSet } from './FilterGroup.svelte';
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
 import { Tab, TabList, TabPanel, Tabs } from './tabs/tabs';
+export { Tab, TabList, TabPanel, Tabs };
+import { FilterGroup, FilterRow, FilterSet } from './filter/filter';
+export { FilterGroup, FilterRow, FilterSet };
 
 export { default as Switch } from './Switch.svelte';
 export { default as Button } from './Button.svelte';
@@ -10,9 +13,6 @@ export { default as HoverTooltip } from './HoverTooltip.svelte';
 export { default as Jazzicon } from './Jazzicon.svelte';
 export { default as Select } from './Select.svelte';
 export { default as Modal } from './Modal.svelte';
-
-// export { Tab, TabList, TabPanel, Tabs } from './tabs/tabs';
-export { Tab, TabList, TabPanel, Tabs };
 
 // parser stuff
 export { default as Parser } from './parser/Parser.svelte';
