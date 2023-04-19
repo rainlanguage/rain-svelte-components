cf71600c580708144c2c49f5df27f5e1d953ee43
SocioDroid <sociodroid2019@gmail.com>
2023-04-20 03:21:38 +0530

    Fixed tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { searchForWorkspaceRoot, type UserConfig } from 'vite';
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		globals: true,
-		environment: 'jsdom',
-	},
-	server: {
-		fs: {
-			allow: [searchForWorkspaceRoot(process.cwd())]
-		}
-	},
+	plugins: [sveltekit()]
 };
 
 export default config;
