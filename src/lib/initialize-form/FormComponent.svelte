560910f0c25f4b85548412fb85c6b171bf5574a2
NanezX <victor.nanezj@gmail.com>
2023-07-08 12:08:04 -0400

    handle error: non initializables contracts

--- src/lib/initialize-form/FormComponent.svelte
+++ src/lib/initialize-form/FormComponent.svelte
@@ -1,5 +1,5 @@
 <script context="module" lang="ts">
-	import type { Abi, AbiParameter } from 'abitype';
+	import type { AbiParameter } from 'abitype';
 	export type MethodMeta = AbiParameter & { inputs: any[]; expressions: any[] };
 	export type AbiComponet = AbiParameter & {
 		nameMeta?: string;
@@ -7,113 +7,30 @@
 		isInterpreterField?: boolean;
 		isDeployerField?: boolean;
 	};
-
-	export const findConfigs = (
-		abi_: Abi,
-		contractMeta_: any,
-		handleError_?: Function
-	): AbiComponet[] => {
-		let paths: string[] = [];
-		let mainIndex: number;
-		let entriesIndex: number[];
-
-		// Fill the abi with the meta information.
-		if (contractMeta_ && contractMeta_.methods) {
-			contractMeta_.methods.forEach((method: any) => {
-				if (method.expressions) {
-					method.expressions.forEach((expression: any) => {
-						set(abi_, expression.path + '.nameMeta', expression.name);
-						set(abi_, expression.path + '.descriptionMeta', expression.desc);
-					});
-				}
-				if (method.inputs) {
-					method.inputs.forEach((input: any) => {
-						set(abi_, input.path + '.nameMeta', input.name);
-						set(abi_, input.path + '.descriptionMeta', input.desc);
-					});
-				}
-			});
-		}
-
-		let initializeMethod = (contractMeta_.methods as MethodMeta[]).find(
-			(method_: any) => method_.name == 'initialize'
-		);
-		if (initializeMethod) {
-			initializeMethod.inputs.forEach((input_: any) => paths.push(input_.path));
-			initializeMethod.expressions.forEach((input_: any) => paths.push(input_.path));
-		}
-
-		// Assuming that all the paths shared the same index on the ABI. Otherwise,
-		// the config on the metadata should be wrong.
-		// The code is trying to catch cases were the contract meta is wrong. Try to
-		// maintin the TS typing.
-		const mainIndexMatch = paths?.[0].match(/\[(\d+)]/)?.[1];
-
-		// This way to avoid iterating multiple times.
-		let inputsMatch: string[] = [];
-		paths.forEach((path) => {
-			const v = path.match(/inputs\[(\d+)\]/)?.[1];
-			if (v) inputsMatch.push(v);
-		});
-
-		if (!mainIndexMatch || !inputsMatch.length) {
-			if (handleError_) handleError_();
-			return [];
-		}
-
-		mainIndex = parseInt(mainIndexMatch, 10);
-		entriesIndex = [...new Set(inputsMatch.map((inputI_) => parseInt(inputI_, 10)))];
-
-		const posiAbi = abi_[mainIndex] as MethodMeta;
-
-		// Saving config
-		return posiAbi.inputs.filter((_, i_) => entriesIndex.includes(i_));
-	};
 </script>
 
 <script lang="ts">
-	import { parseInt, set } from 'lodash-es';
 	import AutoAbiFormComponent from '$lib/auto-abi-form/AutoAbiFormComponent.svelte';
 	import { setContext } from 'svelte';
 
-	export let contractMeta: any;
-	export let abi: Abi;
 	export let result: any = [];
+	export let components: AbiComponet[] = [];
 
 	export let onlyExpressions = false;
 	export let onlyConfig = false;
 	export let showInterpreterFields = false;
-	export let pave: any[] = [];
-
-	let errorMeta: boolean;
-	let errorMetaMsg: string;
 
 	setContext('abi-form', {
 		onlyExpressions,
 		onlyConfig,
 		showInterpreterFields
 	});
-
-	let configComponents: AbiComponet[] = findConfigs(abi, contractMeta);
-
-	$: configComponents, (pave = configComponents);
 </script>
 
-{#if contractMeta || !contractMeta}
-	{#if configComponents}
-		<div class="flex flex-col gap-y-4">
-			{#each configComponents as component, i}
-				<AutoAbiFormComponent
-					{component}
-					bind:result={result[i]}
-					on:save
-					on:load
-					on:expand
-					on:help
-				/>
-			{/each}
-		</div>
-	{/if}
-{:else}
-	Invalid contract metadata
+{#if components}
+	<div class="flex flex-col gap-y-4">
+		{#each components as component, i}
+			<AutoAbiFormComponent {component} bind:result={result[i]} on:save on:load on:expand on:help />
+		{/each}
+	</div>
 {/if}
