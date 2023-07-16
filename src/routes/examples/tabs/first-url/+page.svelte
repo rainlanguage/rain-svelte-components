b88b585041e817d38fe84c71c997e511b7cabf3b
NanezX <victor.nanezj@gmail.com>
2023-07-16 18:18:58 -0400

    update redirect example

--- src/routes/examples/tabs/first-url/+page.svelte
+++ src/routes/examples/tabs/first-url/+page.svelte
@@ -1,23 +1,17 @@
 <script>
+	import { goto } from '$app/navigation';
 	const navigateToAnchor = () => {
-		const location = window.location;
-		window.location.replace(`${location.origin}${location.pathname.replace("/first-url", "#external-references")}`);
+		goto('/examples/tabs#external-references');
 	};
 </script>
 
 <div>
-  You have been redirected to the URL defined. Click
-  <span
-  class="aver"
-    on:click={navigateToAnchor}
-    on:keydown={navigateToAnchor}
-  >
-    here
-  </span> to go back.
+	You have been redirected to the URL defined. Click
+	<button class="here-button" on:click={navigateToAnchor}> here </button> to go back.
 </div>
 
 <style lang="postcss">
-  .aver {
-    @apply cursor-pointer underline underline-offset-2 hover:text-gray-500;
-  }
+	.here-button {
+		@apply underline underline-offset-2 hover:text-gray-500;
+	}
 </style>
