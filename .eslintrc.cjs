097b98af273892e659e92b51944aa1e6a3678f22
NanezX <victor.nanezj@gmail.com>
2023-07-16 18:46:43 -0400

    eslint config with new plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,35 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:svelte/recommended'
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	// plugins: ['svelte3', '@typescript-eslint'],
+	// overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	// settings: {
+	// 	'svelte3/typescript': () => require('typescript')
+	// },
+
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: './tsconfig.json',
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
